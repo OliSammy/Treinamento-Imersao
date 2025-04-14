4274fdf36588881d22ec53ef3e486e1c11b1deb2
Dvt55 <dmelo8185@gmail.com>
2025-04-14 15:16:15 -0300

    Adicionando David Melo ao Arquivo

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Bem-vindos alunos da imersão
 # Criem usa branch, modifiquem o arquivo com seu nome, suba para o repositório.
 
-print("Aluno 1")
+print("David Melo")
 print("Aluno 2")
 print("Aluno 3")
 print("Aluno 4")
