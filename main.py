8bc0fdc9f9c757d3c955caae22c8a8d1a6657582
Samuel Oliveira <samuel.oliveira@hp.com>
2025-04-14 14:19:14 -0300

    Subindo os print

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 # Bem-vindos alunos da imersão
 # Criem usa branch, modifiquem o arquivo com seu nome, suba para o repositório.
+
+print("Aluno 1")
+print("Aluno 2")
+print("Aluno 3")
+print("Aluno 4")
+print("Aluno 5")
+print("Aluno 6")
